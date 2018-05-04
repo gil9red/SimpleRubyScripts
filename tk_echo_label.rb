1811920e2ed2e6b96c4f339d8ef8c15a5a4858c3
ipetrash <ilya.petrash@inbox.ru>
2018-05-04 10:59:39 +0500

    Append tk_echo_label.rb

---
+++ tk_echo_label.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+require 'tk'
+
+app = TkRoot.new {
+  title "Window"
+}
+
+line_edit1 = TkEntry.new(app) { pack {} }
+label1 = TkLabel.new(app) { pack {} }
+label2 = TkLabel.new(app) { pack{} }
+
+line_edit1.bind("KeyRelease", proc {
+  text = line_edit1.get
+  label1['text'] = text
+  label2['text'] = text.reverse
+})
+
+Tk.mainloop
