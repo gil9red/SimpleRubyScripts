c96cffb814791856b91c0219808cee785cfc6f7f
ipetrash <ilya.petrash@inbox.ru>
2018-05-05 03:41:05 +0500

    #

--- hello_world.rb
+++ hello_world.rb
@@ -6,3 +6,19 @@ text = 'RUBY'
 puts "Hello " + text + "!"
 puts "Hello %s!" % [text]
 puts "Hello #{text}!"
+puts
+
+class Foo
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+
+  def say()
+    puts "Hello #{@name}!"
+  end
+end
+
+d = Foo.new("Ruby")
+puts "Hello #{d.name}!"
+d.say()
