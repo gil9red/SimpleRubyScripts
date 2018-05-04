9cd75b6a1100b7f4ef0ebd8262fa2fe9765e7ec4
ipetrash <ilya.petrash@inbox.ru>
2018-05-04 12:41:32 +0500

    #

--- get_ruby_keywords.rb
+++ get_ruby_keywords.rb
@@ -11,3 +11,9 @@ puts "keywords:\n" + keywords.join(", ")
 # BEGIN, END, __FILE__, __LINE__, alias, and, begin, break, case, class, def, defined?, do, else, elsif, end,
 # ensure, false, for, if, in, module, next, nil, not, or, redo, rescue, retry, return, self, super, then, true,
 # undef, unless, until, when, while, yield
+
+print "keywords: %s\n" % [keywords]
+# keywords: ["BEGIN", "END", "__FILE__", "__LINE__", "alias", "and", "begin", "break", "case", "class", "def",
+# "defined?", "do", "else", "elsif", "end", "ensure", "false", "for", "if", "in", "module", "next", "nil", "not",
+# "or", "redo", "rescue", "retry", "return", "self", "super", "then", "true", "undef", "unless", "until", "when",
+# "while", "yield"]
