ff80b3c4c48cb3aab93726ca13f148f2c8474c27
ipetrash <ilya.petrash@inbox.ru>
2018-05-04 11:35:12 +0500

    #

--- tk_echo_label.rb
+++ tk_echo_label.rb
@@ -2,9 +2,21 @@
 
 require 'tk'
 
+def center_window(root, width=300, height=200)
+    # get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # calculate position x and y coordinates
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    root.geometry('%dx%d+%d+%d' % [width, height, x, y])
+end
+
 app = TkRoot.new {
   title "Window"
 }
+center_window(app)
 
 line_edit1 = TkEntry.new(app) { pack {} }
 label1 = TkLabel.new(app) { pack {} }
--- tk_my_public_ip.rb
+++ tk_my_public_ip.rb
@@ -4,9 +4,21 @@ require 'tk'
 require 'open-uri'
 require 'json'
 
+def center_window(root, width=300, height=200)
+    # get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # calculate position x and y coordinates
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    root.geometry('%dx%d+%d+%d' % [width, height, x, y])
+end
+
 app = TkRoot.new {
   title "Window"
 }
+center_window(app)
 
 label = TkLabel.new(app) {
   text '<Unknown ip>'
