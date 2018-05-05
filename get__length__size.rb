8aad1030a906d6ee9a3428980dd7f7e9bbe221c6
ipetrash <ilya.petrash@inbox.ru>
2018-05-05 16:58:04 +0500

    Append get__length__size.rb

---
+++ get__length__size.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+
+# NOTE: Work with: #length and #size
+
+puts '1234'.length
+puts [1, 2, 3, 4].length
+puts({1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd'}.length)
+puts
+
+puts '1234'.size
+puts [1, 2, 3, 4].size
+puts({1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd'}.size)
