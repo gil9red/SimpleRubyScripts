b9f5a7c414846f5d534747758144e407ca6f225e
ipetrash <ilya.petrash@inbox.ru>
2018-05-06 02:11:46 +0500

    #

--- list__range__example.rb
+++ list__range__example.rb
@@ -3,8 +3,9 @@
 
 # List
 items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-p items
+p items  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# +1+2+3+4+5+6+7+8+9
 items.each do |i|
   print '+' + i.to_s
 end
@@ -14,6 +15,10 @@ puts
 p 'Hello Ruby!'.split('').map { |i| i + '!' }
 p [1, 2, 3, 4, 5].map { |i| i ** 2 }
 p [1, 2, 3, 4, 5].map { |i| i ** 2 }.map { |i| i.to_s }
+puts
+
+# "0, 1, 4, 9, 16, 25, 36, 49, 64, 81"
+p 10.times.map{ |i| i ** 2 }.join(', ')
 puts
 
 # Range
