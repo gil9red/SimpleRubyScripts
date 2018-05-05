39bf50b68a20d319a2beb74eb2e3d403b4057c48
ipetrash <ilya.petrash@inbox.ru>
2018-05-05 16:40:12 +0500

    #

--- multiline_string_literal.rb
+++ multiline_string_literal.rb
@@ -8,12 +8,17 @@ Hello World!
 .
 MY_TEXT
 
-text2 = <<MY_TEXT
-1 + 1 = 2
-MY_TEXT
-
 puts text
 puts text.inspect
 puts
 
+text2 = <<MY_TEXT
+1 + 1 = 2
+MY_TEXT
+
+text3 = <<X
+2 + 2 * 2 = #{2 + 2 * 2}
+X
+
 puts text2
+puts text3
