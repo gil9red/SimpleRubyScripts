3d8c9e20a4b58d525fe5e5c918141241413d1c05
ipetrash <ilya.petrash@inbox.ru>
2018-05-06 02:50:29 +0500

    #

--- string__replace__sub__gsub.rb
+++ string__replace__sub__gsub.rb
@@ -10,6 +10,6 @@ puts value.sub("abc", "---")              # --- abc
 puts value.sub(/ab./, "---")              # --- abc
 puts
 
-# Gsub replaces all instances.
+# Gsub replaces all instances. G -- global. In java: replace and replaceAll
 puts value.gsub("abc", "---")             # --- ---
 puts value.gsub(/ab./, "---")             # --- ---
