bfaf2d874a89958135a4c821c20eec0f526c7a6e
ipetrash <ilya.petrash@inbox.ru>
2018-05-06 02:08:56 +0500

    #

--- list__example.rb
+++ list__example.rb
@@ -2,7 +2,14 @@
 
 
 # List
-p [1, 2, 3, 4, 5, 6, 7, 8, 9]
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+p items
+
+items.each do |i|
+  print '+' + i.to_s
+end
+puts
+
 puts
 p 'Hello Ruby!'.split('').map { |i| i + '!' }
 p [1, 2, 3, 4, 5].map { |i| i ** 2 }
