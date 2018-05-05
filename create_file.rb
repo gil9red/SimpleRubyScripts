c49602f6b3e0111cf3b082c266f0baab5f27e0ea
ipetrash <ilya.petrash@inbox.ru>
2018-05-05 23:13:42 +0500

    #

--- create_file.rb
+++ create_file.rb
@@ -8,3 +8,10 @@ File.write('hello world.txt', text)
 File.open("hello world 2.txt", "wb") do |f|
   f.write(text)
 end
+
+
+require 'open-uri'
+
+open("hello world 3.txt", 'wb') do |file|
+  file << text
+end
