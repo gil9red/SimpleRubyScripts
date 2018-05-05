faf609a9d5710a4a9b2362a646f15c4172bfeede
ipetrash <ilya.petrash@inbox.ru>
2018-05-05 17:47:52 +0500

    #

--- parsing_url__html__by_xpath_and_css.rb
+++ parsing_url__html__by_xpath_and_css.rb
@@ -8,7 +8,8 @@ require 'open-uri'
 # SOURCE: https://github.com/sparklemotion/nokogiri#synopsis
 
 # Fetch and parse HTML document
-doc = Nokogiri::HTML(open('http://www.nokogiri.org/tutorials/installing_nokogiri.html'))
+html_text = open('http://www.nokogiri.org/tutorials/installing_nokogiri.html')
+doc = Nokogiri::HTML(html_text)
 
 puts "### Search for nodes by css"
 doc.css('nav ul.menu li a', 'article h2').each do |link|
