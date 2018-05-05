55d6f77cc7b99155368e5f3ebbf5b017eb1e3e23
ipetrash <ilya.petrash@inbox.ru>
2018-05-05 16:46:40 +0500

    #

--- parsing__html.rb
+++ parsing__html.rb
@@ -18,7 +18,8 @@ doc = Nokogiri::HTML(html_str)
 puts doc
 puts
 
-puts doc.at_xpath('//title')
-puts doc.at_xpath('//title').content
-puts doc.at_xpath('//title/text()')
-puts doc.at_xpath('/html/head/title/text()')
+puts 'TITLE: ' + doc.at_xpath('//title')
+puts 'TITLE: ' + doc.at_xpath('//title').content
+puts 'TITLE: ' + doc.at_xpath('//title').text
+puts 'TITLE: ' + doc.at_xpath('//title/text()')
+puts 'TITLE: ' + doc.at_xpath('/html/head/title/text()')
