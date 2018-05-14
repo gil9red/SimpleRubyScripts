bd02454c3b34630798465212917ad76a960bdc89
ipetrash <ilya.petrash@inbox.ru>
2018-05-14 20:52:02 +0500

    #

--- FOO_TEST_TEST.rb
+++ FOO_TEST_TEST.rb
@@ -6,3 +6,30 @@
 #puts 'Hello World'
 
 # TODO: logger
+# TODO: empty
+# TODO: continue / next    https://stackoverflow.com/questions/4010039/equivalent-of-continue-in-ruby
+# TODO: join url
+# TODO: base file name
+
+##p 'Hello World  '.empty?
+##p ''.empty?
+##p ''.to_s.empty?
+##
+##exit
+#
+#require 'nokogiri'
+#require 'open-uri'
+#
+#
+##File.basename
+##"//upload.wikimedia.org/wikipedia/commons/thumb/2/21/Speaker_Icon.svg/15px-Speaker_Icon.svg.png"
+##"/static/images/wikimedia-button.png"
+##"/static/images/poweredby_mediawiki_88x31.png"
+##
+##path = "/path/to/xyz.mp4"
+##
+##File.basename(path)         # => "xyz.mp4"
+##File.extname(path)          # => ".mp4"
+##File.basename(path, ".mp4") # => "xyz"
+##File.basename(path, ".*")   # => "xyz"
+##File.dirname(path)          # => "/path/to"
